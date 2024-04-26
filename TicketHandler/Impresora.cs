e604df6652f7f61e6693ee182e1e93daba2fb4bb
Cristian Leal <projects@sinergicotechnology.com>
2024-04-26 09:37:08 +0100

    Update Impresora.cs

--- TicketHandler/Impresora.cs
+++ TicketHandler/Impresora.cs
@@ -409,7 +409,89 @@ namespace TicketHandler
         /// <param name="stream"></param>
         /// <param name="ticket"></param>
         /// <param name="viasPago"></param>
-        private static void PrintProductsAndPayments(this MemoryStream stream, Document ticket, List<ViaPago> viasPago)
+        private static void PrintProductsAndPaymentsTecPV(this MemoryStream stream, Document ticket, List<ViaPago> viasPago)
+        {
+
+            stream.TextoNegrita();
+            stream.WriteLn($"Codigo    Descrip.        Cant.    Precio    Dto%    Dto E    IGIC    Importe", 1);
+            stream.TextoDefecto();
+            stream.Separator();
+            ticket.DocumentLines.ForEach(line =>
+            {
+                //string itemName = new string(line.ItemName.Take(16).ToArray());
+                stream.WriteLn($" {line.ItemCode} " +
+                    $"    {line.ItemName} " +
+                    $"        {TicketHandlerUtils.FormatAsMoney(line.Quantity)} " +
+                    $"    {line.PrecioUnitario}€ " +
+                    $"    {line.PorcentajeDescuento} " +
+                    $"    {line.ImporteDescuento} " +
+                    $"    {line.ImporteIGIC}  " +
+                    $"    {TicketHandlerUtils.FormatAsMoney(line.ImporteTotal)}€",
+                    1);
+            });
+            stream.TextoDefecto();
+            stream.Separator();
+            stream.WriteLn("", 4);
+            var importSubTotal = "SUBTOTAL:".PadLeft(60) + (ticket.DocumentHeader.TipoDocumento == DOCTYPE_ABONO ? "-" : "") + ticket.DocumentHeader.ImporteTotal.ToString().PadLeft(15);
+
+            stream.TextoNegrita();
+            stream.WriteLn(importSubTotal + "€", 1);
+
+
+            stream.TextoDefecto();
+            stream.Separator();
+
+            stream.TextoNegrita();
+            stream.WriteLn("Base Imp.        %IGIC           Imp. IGIC" + "Total".PadLeft(25), 1);
+
+            stream.TextoDefecto();
+
+            var impuestosOrdenados = ticket.DocumentLines.GroupBy(x => x.Impuesto);
+            var importeAcumulado = ticket.DocumentHeader.Importe;
+            impuestosOrdenados.ToList().ForEach(lineaImpuesto =>
+            {
+                var importe = lineaImpuesto.Sum(l => l.Importe);
+                var porcentajeImpuesto = lineaImpuesto.First().Impuesto;
+                var igic = lineaImpuesto.Sum(i => i.ImporteIGIC);
+                importeAcumulado += igic;
+                stream.WriteLn($" {importe}      " +
+                    $"        {porcentajeImpuesto}%               " +
+                    $"           {igic}€                                     " +
+                    $"{TicketHandlerUtils.FormatAsMoney(importeAcumulado.GetValueOrDefault(Decimal.Zero))}€",
+                    1);
+            });
+
+            stream.WriteLn($"{ticket.DocumentHeader.ImporteTotal}€".PadLeft(60), 1);
+
+            ticket.DocumentPayments.ForEach(payment =>
+            {
+                ViaPago viaPago = viasPago.FirstOrDefault(paymentType => payment.EntryViaPago == paymentType.CreditCard);
+                if (viaPago != null)
+                {
+                    var pago = "".PadLeft(60) + viaPago.Nombre.ToUpper() + (((ticket.DocumentHeader.TipoDocumento == DOCTYPE_ABONO ? "-" : "") + payment.Importe.ToString())).PadLeft(15);
+                    stream.Write(GetBytes(pago));
+                    stream.Write(Euro());
+                }
+                stream.SkipLines(1);
+                var entregado = "".PadLeft(45) + "ENTREGADO: " + payment.Entregado.ToString().PadLeft(14) + "€";
+                stream.WriteLn(entregado, 1);
+                if (payment.Cambio != Decimal.Zero)
+                {
+                    var cambio = "CAMBIO: ".PadLeft(45) + payment.Cambio.ToString().PadLeft(14) + "€";
+                    stream.WriteLn(cambio, 1);
+                }
+            });
+
+            stream.SkipLines(1);
+        }
+
+        /// <summary>
+        /// Función para imprimir los productos, vías de pago e importes
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="ticket"></param>
+        /// <param name="viasPago"></param>
+        private static void PrintProductsAndPaymentsTecMovil(this MemoryStream stream, Document ticket, List<ViaPago> viasPago)
         {
 
             stream.TextoNegrita();
