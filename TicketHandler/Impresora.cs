bc716075473aca1cfcbfef049ae547daa4a50c0d
Cristian Leal <projects@sinergicotechnology.com>
2024-07-11 16:36:15 +0100

    comprobar si las lineas de empresa son nulas

--- TicketHandler/Impresora.cs
+++ TicketHandler/Impresora.cs
@@ -104,7 +104,10 @@ namespace TicketHandler
 
                 if (printFooter)
                 {
-                    printer.PrintCompanyText(footerLines);
+                    if(footerLines != null)
+                    {
+                        printer.PrintCompanyText(footerLines);
+                    }
 
                     printer.SkipLines(3);
 
